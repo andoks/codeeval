85a8e83d45d262dbb8045f1f7b1e92bf17f5a6ce
andoks <core12@gmail.com>
2017-10-01 05:43:36 +0200

    add hard 234 solution

---
+++ hard_234/hard234.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python3
+
+from typing import List, Dict, Tuple
+
+def get_chars(line: str) -> List[str]:
+    return [x for x in line]
+
+def count_chars(char_list: List[str]) -> Dict[str, int]:
+    charToCount = {}
+    for char in char_list:
+        if char in charToCount:
+            charToCount[char] += 1
+        else:
+            charToCount[char] = 1
+
+    return charToCount
+
+def build_huffman_tree(charsToCount: Dict[str, int]):
+    tree = make_tree_from_dict(charsToCount)
+    while len(tree) > 1:
+        tree = sorted(tree, key=lambda node: node[1][0])
+        left = tree[0]
+        right = tree[1]
+        tree.remove(left)
+        tree.remove(right)
+        left, right = (left, right) if is_left_right_ok(left, right) else (right, left)
+        combined = make_combined_node(left, right)
+        tree.append(combined)
+
+    return tree[0] if len(tree) == 1 else None
+
+Node = Tuple[str, Tuple[int, 'Node', 'Node']]
+
+def is_left_right_ok(left: Node, right: Node) -> bool:
+    if left[1][0] < right[1][0]:
+        return True
+    elif left[1][0] == right[1][0] and left[0] < right[0]:
+        return True
+    else:
+        return False
+
+def make_tree_from_dict(charsToCount: Dict[str, int]) -> List[Node]:
+    tree = [(key, (charsToCount[key], None, None)) for key in charsToCount]
+    return tree
+
+def make_combined_node(left, right):
+    return (left[0] + right[0], (left[1][0]+right[1][0], left, right))
+
+def get_codes_for_nodes(tree: Node, code_so_far: str = "") -> Dict[str, str]:
+    codes = {}
+    left = tree[1][1]
+    right = tree[1][2]
+    if left == None and right == None:
+        return {tree[0]: code_so_far}
+    else:
+        if left != None:
+            codes.update(get_codes_for_nodes(left, code_so_far + "0"))
+        if right != None:
+            codes.update(get_codes_for_nodes(right, code_so_far + "1"))
+
+        return codes
+
+def make_ordered_output_string(charsToCode: Dict[str, str]) -> str:
+    in_order = [n for n in sorted(charsToCode.items(), key=lambda entry: entry[0])]
+    output = ""
+    for entry in in_order:
+        output += entry[0] + ": " + entry[1] + "; "
+
+    return output.strip()
+
+def main():
+    with open(sys.argv[0], 'r') as test_cases:
+        for line in test_cases:
+            characters = get_chars(line)
+            charsToCount = count_chars(characters)
+            huffmanTree = build_huffman_tree(charsToCount)
+            charsToCode = get_codes(huffmanTree)
+            output = make_ordered_output_string(charsToCode)
+            
+            print(output)
+
---
+++ hard_234/test_hard234.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python3
+
+import unittest
+import hard234 as sut
+
+
+class TestHard234(unittest.TestCase):
+    def test_get_chars(self):
+        self.assertEqual([], sut.get_chars(""))
+        self.assertEqual(['a'], sut.get_chars("a"))
+        self.assertEqual(['a', 'b', 'c'], sut.get_chars("abc"))
+        self.assertEqual(['a', ' ', 'b', ' ', 'c'], sut.get_chars("a b c"))
+
+    def test_count_chars(self):
+        self.assertEqual({}, sut.count_chars([]))
+        self.assertEqual({"a": 1}, sut.count_chars(["a"]))
+        self.assertEqual({"a": 1, "b": 1}, sut.count_chars(["a", "b"]))
+        self.assertEqual({"a": 2}, sut.count_chars(["a", "a"]))
+        self.assertEqual({"a": 2, "b": 2}, sut.count_chars(["a", "b", "a", "b"]))
+        self.assertEqual({"a": 2, "b": 2, " ": 3}, sut.count_chars(["a"," ", "b", " ", "a", " ", "b"]))
+
+    def test_is_left_right_ok(self):
+        a = ("a", (1, None, None))
+        b = ("b", (1, None, None))
+        self.assertTrue(sut.is_left_right_ok(a, b))
+        self.assertFalse(sut.is_left_right_ok(b, a))
+
+    def test_make_tree_from_dict(self):
+        d1 = [] 
+        self.assertEqual(d1, sut.make_tree_from_dict({}))
+        d2 = [("a", (1, None, None))]
+        self.assertEqual(d2, sut.make_tree_from_dict({"a": 1}))
+        d3 = [("a", (1, None, None)), ("b", (2, None, None))]
+        self.assertEqual(d3, sut.make_tree_from_dict({"a": 1, "b": 2}))
+
+    def test_make_combined_node(self):
+        cn1 = ("ab", (2, ("a", (1, None, None)), ("b", (1, None, None))))
+        self.assertEqual(cn1, sut.make_combined_node(("a", (1, None, None)), ("b", (1, None, None))))
+        left2 = ("a", (2, None, None))
+        rightLeft = ("b", (1, None, None))
+        rightRight = ("c", (1, None, None))
+        right2 = ("bc", (2, rightLeft, rightRight))
+        cn2 = ("abc", (4, left2, right2))
+        self.assertEqual(cn2, sut.make_combined_node(left2, right2))
+
+    def test_build_huffman_tree_empty(self):
+        self.assertEqual(None, sut.build_huffman_tree({}))
+
+    def test_build_huffman_tree_alphabetical_if_equal_weight(self):
+        a = ("a", (1, None, None))
+        b = ("b", (1, None, None))
+        ab = ("ab", (2, a, b))
+        self.assertEqual(ab, sut.build_huffman_tree({"a": 1, "b": 1}))
+        self.assertEqual(ab, sut.build_huffman_tree({"b": 1, "a": 1}))
+
+    def test_build_huffman_tree(self):
+        a = ("a", (1, None, None))
+        b = ("b", (2, None, None))
+        ab = ("ab", (3, a, b))
+        c = ("c", (4, None, None))
+        abc = ("abc", (7, ab, c))
+        self.assertEqual(a, sut.build_huffman_tree({"a": 1}))
+        self.assertEqual(ab, sut.build_huffman_tree({"a": 1, "b": 2}))
+        self.assertEqual(abc, sut.build_huffman_tree({"a": 1, "b": 2, "c": 4}))
+
+    def test_get_codes_for_nodes_leaf_nodes_alphabetical_when_equal_weight(self):
+        a = ("a", (1, None, None))
+        b = ("b", (1, None, None))
+        ab = ("ab", (2, a, b))
+        self.assertEqual({"a": "0", "b": "1"}, sut.get_codes_for_nodes(ab))
+
+    def test_get_codes_for_nodes_leaf_nodes_with_different_weight(self):
+        a = ("a", (1, None, None))
+        b = ("b", (2, None, None))
+        c = ("c", (3, None, None))
+        ab = ("ab", (3, a, b))
+        abc = ("abc", (6, ab, c))
+
+        self.assertEqual({"a": "00", "b": "01", "c": "1"}, sut.get_codes_for_nodes(abc))
+
+    def test_make_ordered_output_string(self):
+        self.assertEqual("a: 00; b: 01; c: 1;", sut.make_ordered_output_string({"a": "00", "b": "01", "c": "1"}))
+
+if __name__ == '__main__':
+    unittest.main()
